fc6bbaa87377c1287382672558c7d9aa52d0ff63
HimNegi18 <negihimu18@gmail.com>
2026-05-16 23:18:09 +0530

    Add missing image assets

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -8,16 +8,16 @@ import lgZoom from "lightgallery/plugins/zoom";
 
 
 import img1 from "../assets/images/gallery/img1.jpeg"
-import img2 from "../assets/images/gallery/img2.jpg"
+import img2 from "../assets/images/gallery/img2.JPG"
 import img3 from "../assets/images/gallery/img3.jpeg"
-import img4 from "../assets/images/gallery/img4.jpg"
+import img4 from "../assets/images/gallery/img4.JPG"
 import img5 from "../assets/images/gallery/img5.jpeg"
-import img6 from "../assets/images/gallery/img6.jpg"
+import img6 from "../assets/images/gallery/img6.JPG"
 import img7 from "../assets/images/gallery/img7.jpeg"
-import img8 from "../assets/images/gallery/img8.jpg"
+import img8 from "../assets/images/gallery/img8.JPG"
 import img9 from "../assets/images/gallery/img9.jpeg"
-import img10 from "../assets/images/gallery/img10.jpg"
-import img11 from "../assets/images/gallery/img11.jpg"
+import img10 from "../assets/images/gallery/img10.JPG"
+import img11 from "../assets/images/gallery/img11.JPG"
 
 const images = [
   { src: img1 },
